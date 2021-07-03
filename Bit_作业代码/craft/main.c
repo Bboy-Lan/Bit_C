1d0ca9aa4c96210efb9a837924a24c88a9ccb528
Bboy_Lan <948589783@qq.com>
2021-07-03 17:32:16 +0800

    复习博客内容

--- Bit_作业代码/craft/main.c
+++ Bit_作业代码/craft/main.c
@@ -1,25 +1,59 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
+extern int MAX;
+
 int main()
 {
-	
-	//新的内容 打印
-	//写一个代码打印1 - 100之间所有3的倍数的数字
-	// 取模  和  取余数的  运用区别
 
-	//明天开始 肝
-	int i = 0;
-	for (i = 1; i <= 100; i++)
-	{
-		if (i % 3 == 0)
-		{
-			printf("%d ", i);
-		}
 
-	}
+	//判断一个整数是否能5整除  
+	//输入  包括一个整数M（1≤M≤100, 000）
+	//输出  包括一行，如果M可以被5整除就输出YES，否则输出NO（结果大小写敏感）
+	int M = 0;
+	scanf("%d", &M);
+	if (M % 5 == 0)//能被5整除 说明最后取得余数为 0   % 为取余数（也称为取模）
+		printf("YES");
+	else
+		printf("No");
+
 	
-	
+
+
+
+
+
+	//printf("%d\n", strlen("C:\168\tasks\bboy.txt"));
+	//strlen是求字符串的长度 应该是字节
+
+
+	//char ch1[] = { "abcdefg" };
+	//char ch2[] = { 'a','b','c','d','e','f','g'};
+
+	//printf("%s\n", ch1);
+	//printf("%s\n", ch2);
+
+	//printf("ch1= %d\n", strlen(ch1));
+	//printf("ch2= %d\n", strlen(ch2));
+
+
+
+
+
+
+	////当需要打印一个单引号时
+	//char arr1[] = {'\''};
+	//printf("%s\n",arr1);
+	//
+	////当需要打印一个双引号时
+	//char arr2[] = {"\""};
+	//printf("%s\n",arr2);
+
+
+
+	//int arr[10];
+	//printf("%c\n", 90);
+	//printf("%d\n",'Z');
 	
 	return 0;
 
