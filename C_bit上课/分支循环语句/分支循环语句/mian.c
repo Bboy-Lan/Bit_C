8f55dbb0ec912802a6889e7a9c7b57c156e78987
Bboy_Lan <948589783@qq.com>
2021-07-08 23:31:28 +0800

    分支循环1

--- C_bit上课/分支循环语句/分支循环语句/mian.c
+++ C_bit上课/分支循环语句/分支循环语句/mian.c
@@ -1,51 +1,72 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
-//void test()
-//{
-//	static int a = 1;
-//	a = a + 1;
-//	printf("%d ", a);
-//}
 
-//#define MAX(x,y) (x > y ? x : y)
-
-enum level//枚举常量
-{
-	one,
-	two,
-	three
-};
 int main()
 {
 
-	printf("%d\n", one);
-	printf("%d\n", two);
-	printf("%d\n", three);
-
-	//int a = 1 , b = 2;
-	//int c = MAX (a , b);
-	//
-	//printf("%d\n", c);
 
 
-	//int arr[MAX];
-	//
-	//MAX = 20;
-	//
+	int ch;
+	while ((ch = getchar()) != EOF)//直到出现Ctrl+z结束
+	{
+
+		if (ch < '0' || ch > '9')//逻辑或  满足一个条件就执行 
+		{
+			continue;// 这意思是 只要不是 0-9 范围内的字符  就继续循环执行
+		}
+		putchar(ch);//放在 if 里面 外面是有区别的 放在里面 就相当于永远执行不了
+
+	}
 
 
-	//int i = 0;
-	//while (i < 10)
-	//{
-	//	test();
-	//	i += 1;
-	//}
 
 
-	//ushort a = 1;
-	//printf("%d\n", sizeof(ushort));//2
-	//	 
+
+	int ch = 0;//为什么数据类型是 int 
+	while ((ch = getchar()) != EOF)// EOF 返回值 -1  当getchar（）获取失败时 返回EOF -1  本质上讲 键盘上的字符都是十进制整数型 因此是 int 
+	{
+		putchar(ch);
+	}
+
+
+	//switch case 语句应用
+
+	int n = 5;
+	scanf("%d", &n);
+	switch (n)
+	{
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+		printf("工作日");
+		break;
+	case 6:
+	case 7:
+		printf("休息日");
+		break;
+	default:
+		printf("输入指令有误!");
+		break;
+
+
+	}
+
+	// 养成 写 break 的习惯  注意一下 switch格式
+
+
+
+
+	//判断 1-100 之间 的奇数
+
+	int i;
+	for (i = 1; i <= 100; i++)// for 循环的地方 都能用while 解决 只是难易及表达复杂程度不同
+	{
+		if (i % 2 != 0)
+			printf("%d ", i);
+	}
 
 
 
--- C_bit上课/分支循环语句/草稿/main.c
+++ C_bit上课/分支循环语句/草稿/main.c
@@ -2,28 +2,39 @@
 
 #include<stdio.h>
 
-struct information //  结构体变量 
-{
-	char name[20];
-	int age;
-	float height;
-	char sex[4];
-};
+//struct information //  结构体变量 
+//{
+//	char name[20];
+//	int age;
+//	float height;
+//	char sex[4];
+//};
+//struct student
+//{
+//	char name[10];
+//	int age;
+//	char ID[20];
+//	char tel[15];
+//};
 int main()
 {
+	
 
-	struct information i1 = { "张三", 18 , 1.78 , "男" };
-	// . 引用操作符
-	printf("姓名：%s 年龄：%d 身高：%.2f 性别：%s\n", i1.name, i1.age, i1.height, i1.sex); 
-	
-																 
-	struct information* pa = &i1;// pa 即为指针变量
-	// 解引用 *pa  就是原本的 i1  *pa = il
-	printf("姓名：%s 年龄：%d 身高：%.2f 性别：%s\n", (*pa).name, (*pa).age, (*pa).height, (*pa).sex);
-	// 这两种表达方式都是一样的
-	
-	//指向操作符  ->
-	printf("姓名：%s 年龄：%d 身高：%.2f 性别：%s\n", pa->name, pa->age, pa->height, pa->sex);
+
+
+
+	//struct information i1 = { "张三", 18 , 1.78 , "男" };
+	//// . 引用操作符
+	//printf("姓名：%s 年龄：%d 身高：%.2f 性别：%s\n", i1.name, i1.age, i1.height, i1.sex); 
+	//
+	//															 
+	//struct information* pa = &i1;// pa 即为指针变量
+	//// 解引用 *pa  就是原本的 i1  *pa = il
+	//printf("姓名：%s 年龄：%d 身高：%.2f 性别：%s\n", (*pa).name, (*pa).age, (*pa).height, (*pa).sex);
+	//// 这两种表达方式都是一样的
+	//
+	////指向操作符  ->
+	//printf("姓名：%s 年龄：%d 身高：%.2f 性别：%s\n", pa->name, pa->age, pa->height, pa->sex);
 	
 	
 	return 0;
